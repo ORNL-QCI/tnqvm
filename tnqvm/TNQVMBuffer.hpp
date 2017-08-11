5656539cd01384de1ec3f53ba4253b8ee610dcc5
Alex McCaskey <mccaskeyaj@ornl.gov>
2017-08-11 20:22:06 +0000

    Fixing getExpectationValueZ const bug

--- tnqvm/TNQVMBuffer.hpp
+++ tnqvm/TNQVMBuffer.hpp
@@ -50,7 +50,7 @@ public:
         aver_from_manytime = 1.;
     }
 
-    virtual const double getExpectationValueZ() const{
+    virtual const double getExpectationValueZ() {
         return aver_from_wavefunc;
     }
 
