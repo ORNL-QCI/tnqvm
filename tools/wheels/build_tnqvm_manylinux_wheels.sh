0b66473247e99c7dea377a36b9456c60f1e8595a
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-05-03 18:51:15 +0000

    adding lapack/blas to tnqvm wheel
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- tools/wheels/build_tnqvm_manylinux_wheels.sh
+++ tools/wheels/build_tnqvm_manylinux_wheels.sh
@@ -10,5 +10,9 @@ do
         export myPython=/opt/python/$version/bin/python
         export ver=`case $version in "cp36-cp36m") echo 3.6 ;; "cp35-cp35m") echo 3.5 ;; "cp34-cp34m") echo 3.4 ;; "cp33-cp33m") echo 3.3 ;; "cp27-cp27m") echo 2.7 ;; "cp27-cp27mu") echo 2.7 ;; *) echo "invalid";; esac`
         PYTHONPATH=/xacc/build/lib.linux-x86_64-$ver/xacc $myPython setup.py build -t tmp_build --executable="/usr/bin/env python"
+	mkdir -p build/lib.linux-x86_64-$ver/xacc/lib
+	cp /usr/lib64/liblapack.so.3 build/lib.linux-x86_64-$ver/xacc/lib/
+	cp /usr/lib64/libblas.so.3 build/lib.linux-x86_64-$ver/xacc/lib/
+	cp /usr/lib64/libgfortran.so.1 build/lib.linux-x86_64-$ver/xacc/lib
 	PYTHONPATH=/xacc/build/lib.linux-x86_64-$ver/xacc $myPython setup.py bdist_wheel --skip-build
 done
