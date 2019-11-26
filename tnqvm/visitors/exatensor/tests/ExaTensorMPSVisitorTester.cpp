4fb09c3e49b84928b0d56d6d2ee6e773fc2077e7
Thien Nguyen <nguyentm@ornl.gov>
2019-11-26 12:54:18 -0500

    Add boilerplate code for ExaTN visitor and comment out legacy code to make the compiler happy.
    (cherry picked from commit c879d8dea4dec18e8f065568f8182c3e5a5727be)

--- tnqvm/visitors/exatensor/tests/ExaTensorMPSVisitorTester.cpp
+++ tnqvm/visitors/exatensor/tests/ExaTensorMPSVisitorTester.cpp
@@ -1,7 +1,7 @@
 /***********************************************************************************
- * Copyright (c) 2017, UT-Battelle
+ * Copyright (c) 2016, UT-Battelle
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *   * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  *   * Neither the name of the xacc nor the
  *     names of its contributors may be used to endorse or promote products
  *     derived from this software without specific prior written permission.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -23,33 +23,21 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  * Contributors:
- *   Implementation - Dmitry Lyakh 2017/11/04;
- *
- **********************************************************************************/
+ *   Initial implementation - Thien Nguyen
+ * 
+**********************************************************************************/
+#include <gtest/gtest.h>
 
-#include "GateFactory.hpp"
+TEST(ExaTensorMPSVisitorTester, checkSimpleSimulation) 
+{
+    // TODO
+}
 
-namespace tnqvm {
-
-//Static class member storage:
-
-constexpr const GateBodyFactory::TensDataType GateBodyFactory::HBody[ONE_BODY_VOL];
-constexpr const GateBodyFactory::TensDataType GateBodyFactory::XBody[ONE_BODY_VOL];
-constexpr const GateBodyFactory::TensDataType GateBodyFactory::YBody[ONE_BODY_VOL];
-constexpr const GateBodyFactory::TensDataType GateBodyFactory::ZBody[ONE_BODY_VOL];
-constexpr const GateBodyFactory::TensDataType GateBodyFactory::RxBody[ONE_BODY_VOL];
-constexpr const GateBodyFactory::TensDataType GateBodyFactory::RyBody[ONE_BODY_VOL];
-constexpr const GateBodyFactory::TensDataType GateBodyFactory::RzBody[ONE_BODY_VOL];
-constexpr const GateBodyFactory::TensDataType GateBodyFactory::CPBody[TWO_BODY_VOL];
-constexpr const GateBodyFactory::TensDataType GateBodyFactory::CNBody[TWO_BODY_VOL];
-constexpr const GateBodyFactory::TensDataType GateBodyFactory::CZBody[TWO_BODY_VOL];
-constexpr const GateBodyFactory::TensDataType GateBodyFactory::SWBody[TWO_BODY_VOL];
-
-#ifdef TNQVM_HAS_EXATENSOR
-// constexpr const std::size_t GateFactory::OneBodyShape[ONE_BODY_RANK];
-// constexpr const std::size_t GateFactory::TwoBodyShape[TWO_BODY_RANK];
-#endif //TNQVM_HAS_EXATENSOR
-
-} //namespace tnqvm
+int main(int argc, char **argv) 
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  auto ret = RUN_ALL_TESTS();
+  return ret;
+} 
