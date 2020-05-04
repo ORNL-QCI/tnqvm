3584298aa1f831a5340a9b7b26464d2ab79dbaa0
Thien Nguyen <nguyentm@ornl.gov>
2020-05-04 21:00:53 +0000

    Added a test to validate the final distribution
    Using a random circuit and compare the final distribution b/w MPS and direct simulation (both based on ExaTN)
    
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- tnqvm/visitors/exatn-mps/tests/NumericalTester.cpp
+++ tnqvm/visitors/exatn-mps/tests/NumericalTester.cpp
@@ -26,7 +26,7 @@ TEST(NumericalTester, checkNorm)
         std::cout << "Testing no SVD cut-off limit!\n";
         auto accelerator = xacc::getAccelerator("tnqvm", {
             std::make_pair("tnqvm-visitor", "exatn-mps"), 
-            std::make_pair("shots", 10000)
+            std::make_pair("shots", 1)
             // Uncomment to enable detail logging
             // std::make_pair("exatn-logging-level", 1)
         });
@@ -40,20 +40,54 @@ TEST(NumericalTester, checkNorm)
     // With SVD cut-off limit:
     {
         std::cout << "Testing SVD cut-off limit!\n";
-        auto accelerator = xacc::getAccelerator("tnqvm", {std::make_pair("tnqvm-visitor", "exatn-mps"), std::make_pair("shots", 10000), std::make_pair("svd-cutoff", 1e-6)});
+        auto accelerator = xacc::getAccelerator("tnqvm", {std::make_pair("tnqvm-visitor", "exatn-mps"), std::make_pair("shots", 1), std::make_pair("svd-cutoff", 1e-6)});
         auto qreg = xacc::qalloc(NB_QUBITS);
         accelerator->execute(qreg, randomCirc);
         const double norm = (*qreg)["norm"].as<double>();
         EXPECT_NEAR(norm, 1.0, 1e-6);
         //qreg->print();
     }
+}
+
+// Validate the distribution
+TEST(NumericalTester, checkDistribution) 
+{    
+    auto tmp = xacc::getService<xacc::Instruction>("rcs");
+    auto randomCirc = std::dynamic_pointer_cast<xacc::CompositeInstruction>(tmp);
+    const int NB_QUBITS = 5;
+    // Use a large number of shots to get true probability distribution.
+    const int NB_SHOTS = 100000;
+    EXPECT_TRUE(randomCirc->expand({std::make_pair("nq", NB_QUBITS), std::make_pair("nlayers", 15)}));
+    std::cout << "Number of gates = " << randomCirc->nInstructions() << "\n";
+    std::cout << "Circuit:\n" << randomCirc->toString() << "\n";
+
+    // MPS simulation
+    auto acceleratorMps = xacc::getAccelerator("tnqvm", {
+        std::make_pair("tnqvm-visitor", "exatn-mps"), 
+        std::make_pair("shots", NB_SHOTS), 
+        std::make_pair("svd-cutoff", 1e-6)});
+    auto qregMps = xacc::qalloc(NB_QUBITS);
+    acceleratorMps->execute(qregMps, randomCirc);
+    const double norm = (*qregMps)["norm"].as<double>();
+    EXPECT_NEAR(norm, 1.0, 1e-6);
+    qregMps->print();
+    
     // ExaTN: direct tensor contraction (no MPS)
+    auto acceleratorDirect = xacc::getAccelerator("tnqvm", {
+        std::make_pair("tnqvm-visitor", "exatn"), 
+        std::make_pair("shots", NB_SHOTS)
+    });
+
+    auto qregDirect = xacc::qalloc(NB_QUBITS);
+    acceleratorDirect->execute(qregDirect, randomCirc);
+    qregDirect->print();
+
+    for (const auto& sampleBitString: qregMps->getMeasurements())
     {
-        std::cout << "Testing full tensor contraction!\n";
-        auto accelerator = xacc::getAccelerator("tnqvm", {std::make_pair("tnqvm-visitor", "exatn"), std::make_pair("shots", 10000)});
-        auto qreg = xacc::qalloc(NB_QUBITS);
-        accelerator->execute(qreg, randomCirc);
-        // qreg->print();
+        const double probMps = qregMps->computeMeasurementProbability(sampleBitString);
+        const double probDirect = qregDirect->computeMeasurementProbability(sampleBitString);
+        std::cout << sampleBitString << ": " << probMps << " vs. " << probDirect << "\n";
+        EXPECT_NEAR(probMps, probDirect, 0.01);
     }
 }
 
