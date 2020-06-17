b099acb70b80cdf3e30b97dd602ae3da2ad699ff
Thien Nguyen <nguyentm@ornl.gov>
2020-06-17 13:11:13 -0400

    Boilerplate for new MPO visitor
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

---
+++ tnqvm/visitors/exatn-mpo/ExaTnMpoVisitor.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "TNQVMVisitor.hpp"
+#include "tensor_network.hpp"
+
+namespace tnqvm {
+class ExatnMpoVisitor : public TNQVMVisitor
+{
+public:
+    // Constructor
+    ExatnMpoVisitor();
+
+    // Virtual function impls:        
+    virtual void initialize(std::shared_ptr<AcceleratorBuffer> buffer, int nbShots) override;
+    virtual void finalize() override;
+
+    // Service name as defined in manifest.json
+    virtual const std::string name() const override { return "exatn-mpo"; }
+    virtual const std::string description() const override { return "ExaTN MPO Visitor"; }
+    virtual std::shared_ptr<TNQVMVisitor> clone() override { return std::make_shared<ExatnMpoVisitor>(); }
+
+    // one-qubit gates
+    virtual void visit(Identity& in_IdentityGate) override;
+    virtual void visit(Hadamard& in_HadamardGate) override;
+    virtual void visit(X& in_XGate) override;
+    virtual void visit(Y& in_YGate) override;
+    virtual void visit(Z& in_ZGate) override;
+    virtual void visit(Rx& in_RxGate) override;
+    virtual void visit(Ry& in_RyGate) override;
+    virtual void visit(Rz& in_RzGate) override;
+    virtual void visit(T& in_TGate) override;
+    virtual void visit(Tdg& in_TdgGate) override;
+    virtual void visit(CPhase& in_CPhaseGate) override;
+    virtual void visit(U& in_UGate) override;
+    // two-qubit gates
+    virtual void visit(CNOT& in_CNOTGate) override;
+    virtual void visit(Swap& in_SwapGate) override;
+    virtual void visit(CZ& in_CZGate) override;
+    virtual void visit(iSwap& in_iSwapGate) override;
+    virtual void visit(fSim& in_fsimGate) override;
+    // others
+    virtual void visit(Measure& in_MeasureGate) override;
+
+    virtual const double getExpectationValueZ(std::shared_ptr<CompositeInstruction> in_function) override;
+private:
+    class ExaTnTensorFunctor : public talsh::TensorFunctor<exatn::Identifiable>
+    {
+    public:
+        ExaTnTensorFunctor(const std::function<int(talsh::Tensor& in_tensor)>& in_func) :
+            m_func(in_func)
+        {}
+
+        const std::string name() const override { return "default"; }
+        const std::string description() const override { return ""; }
+        virtual void pack(BytePacket& packet) override {};
+        virtual void unpack(BytePacket& packet) override {};
+        virtual int apply(talsh::Tensor& local_tensor) override { return m_func(local_tensor); }
+    private:
+        std::function<int(talsh::Tensor& in_tensor)> m_func;
+    }; 
+};
+} 
