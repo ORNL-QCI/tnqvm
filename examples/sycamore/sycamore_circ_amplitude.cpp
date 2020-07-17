313fdb9c8b89ae523dee24fe691de5a7c39cb3d0
Dmitry I. Lyakh <quant4me@gmail.com>
2020-07-16 20:07:21 -0400

    Added additional handles in sycamore_circ_amplitudes.

--- examples/sycamore/sycamore_circ_amplitude.cpp
+++ examples/sycamore/sycamore_circ_amplitude.cpp
@@ -1,4 +1,4 @@
-// Simulate Sycamore circuit with MPS
+// Computes a single amplitude from a Sycamore circuit via full tensor contraction
 #include "xacc.hpp"
 #include <iostream>
 #include <fstream>
@@ -6,32 +6,33 @@
 
 // Initial state:
 const std::vector<int> INITIAL_STATE_BIT_STRING(53, 0);
+
 std::string bitStringVecToString(const std::vector<int>& in_vec)
 {
     std::stringstream s;
-    for (const auto& bit: in_vec)
-    {
-        s << bit;
-    }
+    for (const auto& bit: in_vec) s << bit;
     return s.str();
 }
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
     xacc::Initialize();
+    //xacc::set_verbose(true);
+    //xacc::setLoggingLevel(2);
+
     // Options: 4, 5, 6, 8, 10, 12, 14, 16, 18, 20
-    const int CIRCUIT_DEPTH = 4; 
-    
+    const int CIRCUIT_DEPTH = 4;
+
    // Construct the full path to the XASM source file
     const std::string XASM_SRC_FILE = std::string(RESOURCE_DIR) + "/sycamore_53_" + std::to_string(CIRCUIT_DEPTH) + "_0.xasm";
     // Read XASM source
     std::ifstream inFile;
-    inFile.open(XASM_SRC_FILE); 
+    inFile.open(XASM_SRC_FILE);
     std::stringstream strStream;
-    strStream << inFile.rdbuf(); 
+    strStream << inFile.rdbuf();
     const std::string kernelName = "sycamoreCirc";
-    std::string xasmSrcStr = strStream.str(); 
-    // Construct a unique kernel name: 
+    std::string xasmSrcStr = strStream.str();
+    // Construct a unique kernel name:
     const std::string newKernelName = kernelName + "_" + std::to_string(CIRCUIT_DEPTH);
     xasmSrcStr.replace(xasmSrcStr.find(kernelName), kernelName.length(), newKernelName);
 
@@ -40,9 +41,10 @@ int main(int argc, char **argv)
     const std::vector<int> BIT_STRING(53, 0);
 
     // ExaTN visitor
-    auto qpu = xacc::getAccelerator("tnqvm", { 
+    auto qpu = xacc::getAccelerator("tnqvm", {
         std::make_pair("tnqvm-visitor", "exatn"),
-        std::make_pair("bitstring", BIT_STRING)
+        std::make_pair("bitstring", BIT_STRING),
+        std::make_pair("exatn-buffer-size-gb", 2)
     });
 
     // Allocate a register of 53 qubits
@@ -67,4 +69,4 @@ int main(int argc, char **argv)
 
     xacc::Finalize();
     return 0;
-} 
+}
