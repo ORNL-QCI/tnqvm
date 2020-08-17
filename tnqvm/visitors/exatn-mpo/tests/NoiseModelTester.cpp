30249ae68ec5c49a9d427c456fb487b518cb3b23
Thien Nguyen <nguyentm@ornl.gov>
2020-08-17 09:51:44 -0400

    Add test to validate gate time mapping
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- tnqvm/visitors/exatn-mpo/tests/NoiseModelTester.cpp
+++ tnqvm/visitors/exatn-mpo/tests/NoiseModelTester.cpp
@@ -25,6 +25,42 @@ TEST(NoiseModelTester, checkReadout)
     EXPECT_NEAR(qreg->computeMeasurementProbability("1"), 0.06499999999999995, 0.01);
 } 
 
+TEST(NoiseModelTester, checkGateTime)
+{
+    const std::string BACKEND_JSON_FILE = std::string(BACKEND_CONFIG_DIR) + "/ibmqx2.json";
+    std::ifstream inFile;
+    inFile.open(BACKEND_JSON_FILE);
+    std::stringstream strStream;
+    strStream << inFile.rdbuf();
+    const std::string json = strStream.str();  
+    auto xasmCompiler = xacc::getCompiler("xasm");
+    auto program1 = xasmCompiler->compile(R"(__qpu__ void testXGate(qbit q) {
+        X(q[0]);
+        Measure(q[0]);
+    })")->getComposite("testXGate");
+
+    // H-Z-H is equivalent to X:
+    // Note: H gate is a *half-length* gate (u2); Z gate is always *noiseless*.
+    // Hence, although there are 3 gates in this circuit, the result is equivalent to 
+    // that of a single X gate. 
+    auto program2 = xasmCompiler->compile(R"(__qpu__ void testXGateTransform(qbit q) {
+        H(q[0]);
+        Z(q[0]);
+        H(q[0]);
+        Measure(q[0]);
+    })")->getComposite("testXGateTransform");
+
+    auto accelerator = xacc::getAccelerator("tnqvm", { { "tnqvm-visitor", "exatn-pmps" }, { "backend-json", json }, { "shots", 8192 } });
+    auto qreg1 = xacc::qalloc(1);
+    accelerator->execute(qreg1, program1);
+    qreg1->print();
+
+    auto qreg2 = xacc::qalloc(1);
+    accelerator->execute(qreg2, program2);
+    qreg2->print();
+    EXPECT_NEAR(qreg1->computeMeasurementProbability("1"), qreg2->computeMeasurementProbability("1"), 0.01);
+} 
+
 int main(int argc, char **argv) 
 {
     xacc::Initialize();
