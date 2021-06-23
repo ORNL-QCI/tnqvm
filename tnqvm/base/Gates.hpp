df4807b457bb963788ac1b05ef8616e7f0c3ef74
Dmitry I. Lyakh <quant4me@gmail.com>
2021-06-23 15:40:26 -0400

    Fixed indentation and removed trailing spaces in a few source files because my text editor colors them.
    Signed-off-by: Dmitry I. Lyakh <quant4me@gmail.com>

--- tnqvm/base/Gates.hpp
+++ tnqvm/base/Gates.hpp
@@ -1,7 +1,7 @@
 /***********************************************************************************
  * Copyright (c) 2016, UT-Battelle
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *   * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  *   * Neither the name of the xacc nor the
  *     names of its contributors may be used to endorse or promote products
  *     derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -23,10 +23,10 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * Contributors:
  *   Initial API and implementation - Alex McCaskey
- * 
+ *
 **********************************************************************************/
 #pragma once
 #include <string>
@@ -35,13 +35,13 @@
 #include <cassert>
 
 namespace tnqvm {
-    // Enum of common quantum gates. 
+    // Enum of common quantum gates.
     // The enum corresponds to a gate name string.
     // See: XACC's CommonGates.hpp
-    enum class CommonGates: int { 
+    enum class CommonGates: int {
         I = 0,
-        H, 
-        CNOT, 
+        H,
+        CNOT,
         Rx,
         Ry,
         Rz,
@@ -63,13 +63,13 @@ namespace tnqvm {
         iSwap,
         fSim,
         // Count of defined common gates
-        GateCount 
+        GateCount
     };
 
-    static const std::array<std::string, static_cast<size_t>(CommonGates::GateCount)> CommonGateNames = { 
+    static const std::array<std::string, static_cast<size_t>(CommonGates::GateCount)> CommonGateNames = {
         "I",
-        "H", 
-        "CNOT", 
+        "H",
+        "CNOT",
         "Rx",
         "Ry",
         "Rz",
@@ -89,11 +89,11 @@ namespace tnqvm {
         "CH",
         "CRZ",
         "iSwap",
-        "fSim"  
+        "fSim"
     };
 
     inline std::string GetGateName(CommonGates in_gateEnum) { return CommonGateNames[static_cast<size_t>(in_gateEnum)]; }
-    
+
     inline CommonGates GetGateType(const std::string& in_gateName)
     {
         for (int i = 0; i < static_cast<int>(CommonGates::GateCount); ++i)
@@ -106,7 +106,7 @@ namespace tnqvm {
         // Invalid
         return CommonGates::GateCount;
     }
-    
+
     inline bool IsControlGate(CommonGates in_gateEnum) {
         switch (in_gateEnum)
         {
@@ -116,109 +116,109 @@ namespace tnqvm {
             case CommonGates::CY:
             case CommonGates::CH:
             case CommonGates::CRZ: return true;
-            default: 
+            default:
                 return false;
         }
     }
 
     template<typename tnqvm::CommonGates, typename... Args>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix(Args... in_gateArgs) {
-        // We don't expect this to be called if template specialization is not provided for a specific gate.         
+        // We don't expect this to be called if template specialization is not provided for a specific gate.
         assert(false);
         return {};
     }
-    
-    template <> 
+
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::I>() {
-        return 
+        return
         {
             { 1.0, 0.0 },
             { 0.0, 1.0 }
-        };    
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::H>() {
-        return 
+        return
         {
             { M_SQRT1_2, M_SQRT1_2 },
             { M_SQRT1_2, -M_SQRT1_2 }
-        };    
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::X>() {
-        return 
+        return
         {
             { 0.0, 1.0 },
             { 1.0, 0.0 }
-        };    
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::Y>() {
-        return 
+        return
         {
             { 0.0, std::complex<double>(0, -1) },
             { std::complex<double>(0, 1), 0.0 }
-        };    
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::Z>() {
-        return 
+        return
         {
             { 1.0, 0.0 },
             { 0.0, -1.0 }
-        };    
+        };
     }
 
     // Rx(theta) gate:
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::Rx>(double in_theta) {
-        return 
+        return
         {
             { std::cos(0.5 * in_theta), std::complex<double>(0, -1) * std::sin(0.5 * in_theta) },
             { std::complex<double>(0, -1) * std::sin(0.5 * in_theta), std::cos(0.5 * in_theta) }
-        };    
+        };
     }
 
     // Ry(theta) gate:
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::Ry>(double in_theta) {
-        return 
+        return
         {
             { std::cos(0.5 * in_theta), -std::sin(0.5 * in_theta) },
             { std::sin(0.5 * in_theta), std::cos(0.5 * in_theta) }
-        };    
+        };
     }
 
     // Rz(theta) gate:
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::Rz>(double in_theta) {
-        return 
+        return
         {
             { std::exp(std::complex<double>(0, -0.5 * in_theta)), 0.0 },
             { 0.0, std::exp(std::complex<double>(0, 0.5 * in_theta)) }
-        };    
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::T>() {
-        return 
+        return
         {
             { 1.0, 0.0 },
             { 0.0, std::exp(std::complex<double>(0, M_PI_4)) }
-        };    
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::Tdg>() {
-        return 
+        return
         {
             { 1.0, 0.0 },
             { 0.0, std::exp(std::complex<double>(0, -M_PI_4)) }
-        };    
+        };
     }
 
     template <>
@@ -234,108 +234,107 @@ namespace tnqvm {
                std::cos(in_theta / 2.0)}};
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::CNOT>() {
-        return 
+        return
         {
             { 1.0, 0.0, 0.0 , 0.0 },
             { 0.0, 1.0, 0.0 , 0.0 },
             { 0.0, 0.0, 0.0 , 1.0 },
             { 0.0, 0.0, 1.0 , 0.0 }
-        };    
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::CZ>() {
-        return 
+        return
         {
             { 1.0, 0.0, 0.0 , 0.0 },
             { 0.0, 1.0, 0.0 , 0.0 },
             { 0.0, 0.0, 1.0 , 0.0 },
             { 0.0, 0.0, 0.0 , -1.0 }
-        };    
+        };
     }
-    
-    template <> 
+
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::CY>() {
-        return 
+        return
         {
             { 1.0, 0.0, 0.0 , 0.0 },
             { 0.0, 1.0, 0.0 , 0.0 },
-            { 0.0, 0.0, 0.0, std::complex<double>(0, -1) },
-            { 0.0, 0.0, std::complex<double>(0, 1), 0.0 }
-        };    
+            { 0.0, 0.0, 0.0, std::complex<double>(0.0, -1.0) },
+            { 0.0, 0.0, std::complex<double>(0.0, 1.0), 0.0 }
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::CH>() {
-        return 
+        return
         {
             { 1.0, 0.0, 0.0 , 0.0 },
             { 0.0, 1.0, 0.0 , 0.0 },
             { 0.0, 0.0, M_SQRT1_2, M_SQRT1_2 },
             { 0.0, 0.0, M_SQRT1_2, -M_SQRT1_2 }
-        };    
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::CRZ>(double in_theta) {
-        return 
+        return
         {
             { 1.0, 0.0, 0.0 , 0.0 },
             { 0.0, 1.0, 0.0 , 0.0 },
-            { 0.0, 0.0, std::exp(std::complex<double>(0, -0.5 * in_theta)) , 0.0 },
-            { 0.0, 0.0, 0.0 , std::exp(std::complex<double>(0, 0.5 * in_theta)) }
-        };    
+            { 0.0, 0.0, std::exp(std::complex<double>(0.0, -0.5 * in_theta)) , 0.0 },
+            { 0.0, 0.0, 0.0 , std::exp(std::complex<double>(0.0, 0.5 * in_theta)) }
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::CPhase>(double in_theta) {
-        return 
+        return
         {
             { 1.0, 0.0, 0.0 , 0.0 },
             { 0.0, 1.0, 0.0 , 0.0 },
             { 0.0, 0.0, 1.0 , 0.0 },
-            { 0.0, 0.0, 0.0 , std::exp(std::complex<double>(0, in_theta)) }
-        };    
+            { 0.0, 0.0, 0.0 , std::exp(std::complex<double>(0.0, in_theta)) }
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::Swap>() {
-        return 
+        return
         {
             { 1.0, 0.0, 0.0 , 0.0 },
             { 0.0, 0.0, 1.0 , 0.0 },
             { 0.0, 1.0, 0.0 , 0.0 },
             { 0.0, 0.0, 0.0 , 1.0 }
-        };    
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::iSwap>() {
-        return 
+        return
         {
             { 1.0, 0.0, 0.0 , 0.0 },
-            { 0.0, 0.0, std::complex<double>(0, 1.0), 0.0 },
-            { 0.0, std::complex<double>(0, 1.0), 0.0 , 0.0 },
+            { 0.0, 0.0, std::complex<double>(0.0, 1.0), 0.0 },
+            { 0.0, std::complex<double>(0.0, 1.0), 0.0 , 0.0 },
             { 0.0, 0.0, 0.0 , 1.0 }
-        };    
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::fSim>(double in_theta, double in_phi) {
-        return 
+        return
         {
             { 1.0, 0.0, 0.0 , 0.0 },
-            { 0.0, std::cos(in_theta), std::complex<double>(0, -std::sin(in_theta)), 0.0 },
-            { 0.0, std::complex<double>(0, -std::sin(in_theta)), std::cos(in_theta), 0.0 },
-            { 0.0, 0.0, 0.0, std::exp(std::complex<double>(0, -in_phi)) }
-        };    
+            { 0.0, std::cos(in_theta), std::complex<double>(0.0, -std::sin(in_theta)), 0.0 },
+            { 0.0, std::complex<double>(0.0, -std::sin(in_theta)), std::cos(in_theta), 0.0 },
+            { 0.0, 0.0, 0.0, std::exp(std::complex<double>(0.0, -in_phi)) }
+        };
     }
 
-    template <> 
+    template <>
     std::vector<std::vector<std::complex<double>>> GetGateMatrix<CommonGates::Measure>() {
-        return {};    
+        return {};
     }
 }
-
--- tnqvm/utils/GateMatrixAlgebra.hpp
+++ tnqvm/utils/GateMatrixAlgebra.hpp
@@ -1,7 +1,7 @@
 /***********************************************************************************
  * Copyright (c) 2016, UT-Battelle
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *   * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  *   * Neither the name of the xacc nor the
  *     names of its contributors may be used to endorse or promote products
  *     derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -23,10 +23,10 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * Contributors:
  *   Initial API and implementation - Thien Nguyen
- * 
+ *
 **********************************************************************************/
 
 // Simple linear algebra quantum gate simulator
@@ -35,13 +35,13 @@
 #include<vector>
 #include <assert.h>
 #include <random>
-#include <chrono> 
+#include <chrono>
 #include <functional>
 
 typedef std::vector<std::complex<double>> StateVectorType;
 typedef std::vector<std::vector<std::complex<double>>> GateMatrixType;
 
-template <typename T> 
+template <typename T>
 std::vector<T> linspace(T a, T b, size_t N) {
   T h = (b - a) / static_cast<T>(N - 1);
   std::vector<T> xs(N);
@@ -49,139 +49,140 @@ std::vector<T> linspace(T a, T b, size_t N) {
   T val;
 
   for (x = xs.begin(), val = a; x != xs.end(); ++x, val += h) {
-	*x = val;
+    *x = val;
   }
-    
+
   return xs;
 }
 
-inline double generateRandomProbability() 
+inline double generateRandomProbability()
 {
-    auto randFunc = std::bind(std::uniform_real_distribution<double>(0, 1), std::mt19937(std::chrono::high_resolution_clock::now().time_since_epoch().count()));
-    return randFunc();
+  auto randFunc = std::bind(std::uniform_real_distribution<double>(0, 1),
+                            std::mt19937(std::chrono::high_resolution_clock::now().time_since_epoch().count()));
+  return randFunc();
 }
 
 void ApplySingleQubitGate(StateVectorType& io_psi, size_t in_index, const GateMatrixType& in_gateMatrix)
 {
-	assert(in_gateMatrix.size() == 2 && in_gateMatrix[0].size() == 2 &&  in_gateMatrix[1].size() == 2);
-    const uint64_t N = io_psi.size();
-	const uint64_t k_range = 1ULL << in_index;
+  assert(in_gateMatrix.size() == 2 && in_gateMatrix[0].size() == 2 &&  in_gateMatrix[1].size() == 2);
+  const uint64_t N = io_psi.size();
+  const uint64_t k_range = 1ULL << in_index;
 
-    auto stateVectorCopy = io_psi;    
-    for (uint64_t g = 0; g < N; g += (k_range * 2))
+  auto stateVectorCopy = io_psi;
+  for (uint64_t g = 0; g < N; g += (k_range * 2))
+  {
+    for (uint64_t i = g; i < g + k_range; ++i)
     {
-        for (uint64_t i = g; i < g + k_range; ++i)
-        {
-            // See https://arxiv.org/pdf/1601.07195.pdf Figure 2 for pseudo-code
-            stateVectorCopy[i] = in_gateMatrix[0][0] * io_psi[i] + in_gateMatrix[0][1] * io_psi[i + k_range];
-            stateVectorCopy[i + k_range] = in_gateMatrix[1][0] * io_psi[i] + in_gateMatrix[1][1] * io_psi[i + k_range];
-        }
+      // See https://arxiv.org/pdf/1601.07195.pdf Figure 2 for pseudo-code
+      stateVectorCopy[i] = in_gateMatrix[0][0] * io_psi[i] + in_gateMatrix[0][1] * io_psi[i + k_range];
+      stateVectorCopy[i + k_range] = in_gateMatrix[1][0] * io_psi[i] + in_gateMatrix[1][1] * io_psi[i + k_range];
     }
+  }
 
-    // Assign the result back to the state vector.
-    io_psi = stateVectorCopy;
+  // Assign the result back to the state vector.
+  io_psi = stateVectorCopy;
 }
 
 void ApplyCNOTGate(StateVectorType& io_psi, size_t in_controlIndex, size_t in_targetIndex)
 {
-    // Must have at least 2 qubits
-    assert(io_psi.size() >= 4);
-    // Qubit index
-    assert(io_psi.size() >= (1ULL << in_targetIndex));
-    assert(io_psi.size() >= (1ULL << in_targetIndex));
+  // Must have at least 2 qubits
+  assert(io_psi.size() >= 4);
+  // Qubit index
+  assert(io_psi.size() >= (1ULL << in_targetIndex));
+  assert(io_psi.size() >= (1ULL << in_targetIndex));
 
-    const uint64_t N = io_psi.size();
-	const uint64_t k_range = 1ULL << in_targetIndex;
-	const uint64_t ctrlmask = 1ULL << in_controlIndex;
-	// Note: the effect of a CNOT gate is a remapping of state vector:
-    // e.g. |1>|0> ==> |1>|1> (first qubit is the control), etc.    
-    if (in_controlIndex > in_targetIndex)
-	{
-		for (uint64_t g = ctrlmask; g < N; g += (ctrlmask * 2))
-		{
-			for (uint64_t i = 0; i < ctrlmask; i += (k_range * 2))
-			{
-				for (uint64_t ii = 0; ii < k_range; ++ii)
-                {
-                    std::swap(io_psi[i + g + ii], io_psi[i + g + ii + k_range]);
-                }
-			}
-		}
-	}
-	else
-	{
-    	for (uint64_t g = 0; g < N; g += (k_range * 2))
-		{
-			for (uint64_t i = ctrlmask; i < k_range; i += (ctrlmask * 2))
-			{
-				for (uint64_t ii = 0; ii < ctrlmask; ++ii)
-                {
-                    std::swap(io_psi[i + g + ii], io_psi[i + g + ii + k_range]);
-                }					
-			}
-		}
-	}
+  const uint64_t N = io_psi.size();
+  const uint64_t k_range = 1ULL << in_targetIndex;
+  const uint64_t ctrlmask = 1ULL << in_controlIndex;
+  // Note: the effect of a CNOT gate is a remapping of state vector:
+  // e.g. |1>|0> ==> |1>|1> (first qubit is the control), etc.
+  if (in_controlIndex > in_targetIndex)
+  {
+    for (uint64_t g = ctrlmask; g < N; g += (ctrlmask * 2))
+    {
+      for (uint64_t i = 0; i < ctrlmask; i += (k_range * 2))
+      {
+        for (uint64_t ii = 0; ii < k_range; ++ii)
+        {
+          std::swap(io_psi[i + g + ii], io_psi[i + g + ii + k_range]);
+        }
+      }
+    }
+  }
+  else
+  {
+    for (uint64_t g = 0; g < N; g += (k_range * 2))
+    {
+      for (uint64_t i = ctrlmask; i < k_range; i += (ctrlmask * 2))
+      {
+        for (uint64_t ii = 0; ii < ctrlmask; ++ii)
+        {
+          std::swap(io_psi[i + g + ii], io_psi[i + g + ii + k_range]);
+        }
+      }
+    }
+  }
 }
 
 template<typename ElementType>
 bool ApplyMeasureOp(std::vector<ElementType>& io_psi, size_t in_qubitIndex)
 {
-    const auto N = io_psi.size();
-    const auto k_range = 1ULL << in_qubitIndex;        
-    const double randProbPick = generateRandomProbability();
-    double cumulativeProb = 0.0;
-    size_t stateSelect = 0;
-    //select a state based on cumulative distribution
-    while (cumulativeProb < randProbPick && stateSelect < N)
-    {
-        cumulativeProb += std::norm(io_psi[stateSelect++]);
-    }
-    stateSelect--;
+  const auto N = io_psi.size();
+  const auto k_range = 1ULL << in_qubitIndex;
+  const double randProbPick = generateRandomProbability();
+  double cumulativeProb = 0.0;
+  size_t stateSelect = 0;
+  //select a state based on cumulative distribution
+  while (cumulativeProb < randProbPick && stateSelect < N)
+  {
+    cumulativeProb += std::norm(io_psi[stateSelect++]);
+  }
+  stateSelect--;
 
-    //take the value of the measured bit
-    bool result = ((stateSelect >> in_qubitIndex) & 1);
-    // Collapse the state vector according to the measurement result
-    double measProb = 0.0;
-    for (size_t g = 0; g < N; g += (k_range * 2))
+  //take the value of the measured bit
+  bool result = ((stateSelect >> in_qubitIndex) & 1);
+  // Collapse the state vector according to the measurement result
+  double measProb = 0.0;
+  for (size_t g = 0; g < N; g += (k_range * 2))
+  {
+    for (size_t i = 0; i < k_range; ++i)
     {
-        for (size_t i = 0; i < k_range; ++i)
-        {
-            if ((((i + g) >> in_qubitIndex) & 1) == result)
-            {
-                measProb += std::norm(io_psi[i + g]);
-                io_psi[i + g + k_range] = 0.0; 
-            }
-            else
-            {
-                measProb += std::norm(io_psi[i + g + k_range]);
-                io_psi[i + g] = 0.0;
-            }
-        }
+      if ((((i + g) >> in_qubitIndex) & 1) == result)
+      {
+        measProb += std::norm(io_psi[i + g]);
+        io_psi[i + g + k_range] = 0.0;
+      }
+      else
+      {
+        measProb += std::norm(io_psi[i + g + k_range]);
+        io_psi[i + g] = 0.0;
+      }
     }
-    // Renormalize the state
-    measProb = std::sqrt(measProb);
-    for (size_t g = 0; g < N; g += (k_range * 2))
+  }
+  // Renormalize the state
+  measProb = std::sqrt(measProb);
+  for (size_t g = 0; g < N; g += (k_range * 2))
+  {
+    for (size_t i = 0; i < k_range; i += 1)
     {
-        for (size_t i = 0; i < k_range; i += 1)
-        {
-            if ((((i + g) >> in_qubitIndex) & 1) == result)
-            {
-                io_psi[i + g] /= measProb;
-            }
-            else
-            {
-                io_psi[i + g + k_range] /= measProb;
-            }    
-        }
+      if ((((i + g) >> in_qubitIndex) & 1) == result)
+      {
+        io_psi[i + g] /= measProb;
+      }
+      else
+      {
+        io_psi[i + g + k_range] /= measProb;
+      }
     }
+  }
 
-    return result;
+  return result;
 }
 
 StateVectorType AllocateStateVector(size_t in_nbQubits)
 {
-    StateVectorType stateVector(1ULL << in_nbQubits);
-    // Default is the |0> state
-    stateVector[0] = 1.0;
-    return stateVector;
+  StateVectorType stateVector(1ULL << in_nbQubits);
+  // Default is the |0> state
+  stateVector[0] = 1.0;
+  return stateVector;
 }
