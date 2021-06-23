5ad90dac7ab68b8b37c2f2df3bd5acf896ff1ee2
Dmitry I. Lyakh <quant4me@gmail.com>
2021-06-23 17:48:32 -0400

    Debugging ExaTN General Visitor ...
    Signed-off-by: Dmitry I. Lyakh <quant4me@gmail.com>

--- tnqvm/visitors/exatn-gen/tests/ExaTnGenTester.cpp
+++ tnqvm/visitors/exatn-gen/tests/ExaTnGenTester.cpp
@@ -149,41 +149,41 @@ TEST(ExaTnGenTester, checkVqeH3) {
   EXPECT_NEAR((*buffer)["opt-val"].as<double>(), -2.04482, 1e-3);
 }
 
-// Disable this test due to timeout on the CI machine.
-// TEST(ExaTnGenTester, checkVqeH3Approx) {
-//   // Reconstruct tensor network every 10 layers
-//   auto accelerator = xacc::getAccelerator(
-//       "tnqvm", {{"tnqvm-visitor", "exatn-gen"}, {"reconstruct-layers", 10}});
-//   xacc::set_verbose(true);
-//   xacc::qasm(R"(
-//         .compiler xasm
-//         .circuit deuteron_ansatz_h3_2
-//         .parameters t0, t1
-//         .qbit q
-//         X(q[0]);
-//         exp_i_theta(q, t0, {{"pauli", "X0 Y1 - Y0 X1"}});
-//         exp_i_theta(q, t1, {{"pauli", "X0 Z1 Y2 - X2 Z1 Y0"}});
-//     )");
-//   auto ansatz = xacc::getCompiled("deuteron_ansatz_h3_2");
-//   auto H_N_3 = xacc::quantum::getObservable(
-//       "pauli",
-//       std::string("5.907 - 2.1433 X0X1 - 2.1433 Y0Y1 + .21829 Z0 - 6.125 Z1 + "
-//                   "9.625 - 9.625 Z2 - 3.91 X1 X2 - 3.91 Y1 Y2"));
-//   auto optimizer = xacc::getOptimizer("nlopt");
-//   // Allocate some qubits and execute
-//   auto buffer = xacc::qalloc(3);
-//   auto vqe = xacc::getAlgorithm("vqe");
-//   vqe->initialize({std::make_pair("ansatz", ansatz),
-//                    std::make_pair("observable", H_N_3),
-//                    std::make_pair("accelerator", accelerator),
-//                    std::make_pair("optimizer", optimizer)});
-//   // The reconstruction can take a long time, so we just test a single
-//   // observable evaluation.
-//   auto energies = vqe->execute(buffer, {0.0684968, 0.17797});
-//   buffer->print();
-//   std::cout << "Energy = " << energies[0] << "\n";
-//   EXPECT_NEAR(energies[0], -2.04482, 0.1);
-// }
+// Disable this test due to timeout on the CI machine
+TEST(ExaTnGenTester, checkVqeH3Approx) {
+  // Reconstruct tensor network every 10 layers
+  auto accelerator = xacc::getAccelerator(
+       "tnqvm", {{"tnqvm-visitor", "exatn-gen"}, {"reconstruct-layers", 10}});
+  xacc::set_verbose(true);
+  xacc::qasm(R"(
+        .compiler xasm
+        .circuit deuteron_ansatz_h3_2
+        .parameters t0, t1
+        .qbit q
+        X(q[0]);
+        exp_i_theta(q, t0, {{"pauli", "X0 Y1 - Y0 X1"}});
+        exp_i_theta(q, t1, {{"pauli", "X0 Z1 Y2 - X2 Z1 Y0"}});
+  )");
+  auto ansatz = xacc::getCompiled("deuteron_ansatz_h3_2");
+  auto H_N_3 = xacc::quantum::getObservable(
+       "pauli",
+       std::string("5.907 - 2.1433 X0X1 - 2.1433 Y0Y1 + .21829 Z0 - 6.125 Z1 + "
+                   "9.625 - 9.625 Z2 - 3.91 X1 X2 - 3.91 Y1 Y2"));
+  auto optimizer = xacc::getOptimizer("nlopt");
+  // Allocate some qubits and execute
+  auto buffer = xacc::qalloc(3);
+  auto vqe = xacc::getAlgorithm("vqe");
+  vqe->initialize({std::make_pair("ansatz", ansatz),
+                   std::make_pair("observable", H_N_3),
+                   std::make_pair("accelerator", accelerator),
+                   std::make_pair("optimizer", optimizer)});
+  // The reconstruction can take a long time, so we just test a single
+  // observable evaluation.
+  auto energies = vqe->execute(buffer, {0.0684968, 0.17797});
+  buffer->print();
+  std::cout << "Energy = " << energies[0] << "\n";
+  EXPECT_NEAR(energies[0], -2.04482, 0.1);
+}
 
 int main(int argc, char **argv) {
   xacc::Initialize(argc, argv);
