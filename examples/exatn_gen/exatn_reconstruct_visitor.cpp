4deae26eb03e1effc9e98fbe41cca8ab19d432b5
Thien Nguyen <thien.md.nguyen@gmail.com>
2022-08-15 17:44:16 +1000

    Add benchmarkExaTnGen4 for Trotter circuit

--- examples/exatn_gen/exatn_reconstruct_visitor.cpp
+++ examples/exatn_gen/exatn_reconstruct_visitor.cpp
@@ -4,6 +4,11 @@
 #include "Optimizer.hpp"
 #include "xacc_observable.hpp"
 #include "Algorithm.hpp"
+#include <iomanip>
+#include "Circuit.hpp"
+#include "CommonGates.hpp"
+#include "InstructionIterator.hpp"
+#include "CountGatesOfTypeVisitor.hpp"
 
 int benchmarkExaTnGen1()
 {
@@ -114,6 +119,59 @@ int benchmarkExaTnGen3()
  return 0;
 }
 
+int benchmarkExaTnGen4() {
+  constexpr int NB_QUBITS = 30;
+  constexpr int NB_TROTTER_STEPS = 10;
+  const std::string HeisenbergHamStr = [&]() {
+    std::stringstream ss;
+    for (int i = 0; i < NB_QUBITS - 1; ++i) {
+      ss << std::fixed << std::setprecision(3) << 1.0 << " X" << i << " X"
+         << i + 1 << " + ";
+      ss << 1.0 << " Y" << i << " Y" << i + 1 << " + ";
+
+      ss << 1.0 << " Z" << i << " Z" << i + 1;
+      if (i != NB_QUBITS - 2) {
+        ss << " + ";
+      }
+    }
+
+    return ss.str();
+  }();
+  //   std::cout << HeisenbergHamStr << "\n";
+  auto HeisenbergHam = xacc::quantum::getObservable("pauli", HeisenbergHamStr);
+  std::cout << HeisenbergHam->toString() << "\n";
+  auto expCirc = std::dynamic_pointer_cast<xacc::quantum::Circuit>(
+      xacc::getService<xacc::Instruction>("exp_i_theta"));
+  auto registry = xacc::getService<xacc::IRProvider>("quantum");
+  auto trotterCirc = registry->createComposite("trotter");
+  const bool okay = expCirc->expand({{"pauli", HeisenbergHamStr}});
+  const int nbCNOTs =
+      std::make_shared<
+          xacc::quantum::CountGatesOfTypeVisitor<xacc::quantum::CNOT>>(expCirc)
+          ->countGates();
+  //   std::cout << "Num CNOT = " << nbCNOTs << "\n";
+
+  for (int i = 0; i < NB_TROTTER_STEPS; ++i) {
+    trotterCirc->addInstructions(
+        xacc::ir::asComposite(expCirc->operator()({1.0})->clone())
+            ->getInstructions());
+  }
+  //   std::cout << "ALL " << trotterCirc->toString() << "\n";
+
+  constexpr int MAX_BOND_DIM = 16;
+  constexpr double RECONSTRUCTION_TOL = 1e-3;
+
+  auto accelerator_exatn_approx = xacc::getAccelerator(
+      "tnqvm", {{"tnqvm-visitor", "exatn-gen"},
+                {"exatn-buffer-size-gb", 2},
+                {"reconstruct-gates", nbCNOTs},
+                {"reconstruct-tolerance", RECONSTRUCTION_TOL},
+                {"max-bond-dim", MAX_BOND_DIM}});
+  auto buffer = xacc::qalloc(NB_QUBITS);
+  accelerator_exatn_approx->execute(buffer, trotterCirc);
+
+  return okay ? 0 : 1;
+}
 
 int main(int argc, char **argv) {
  xacc::Initialize(argc, argv);
@@ -121,9 +179,10 @@ int main(int argc, char **argv) {
  //xacc::logToFile(true);
  //xacc::setLoggingLevel(1);
  int error_code = 0;
- //if(error_code == 0) error_code = benchmarkExaTnGen1();
- //if(error_code == 0) error_code = benchmarkExaTnGen2();
- if(error_code == 0) error_code = benchmarkExaTnGen3();
+ // if(error_code == 0) error_code = benchmarkExaTnGen1();
+ // if(error_code == 0) error_code = benchmarkExaTnGen2();
+ //  if(error_code == 0) error_code = benchmarkExaTnGen3();
+ if (error_code == 0) error_code = benchmarkExaTnGen4();
  xacc::Finalize();
  return error_code;
 }
